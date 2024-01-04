537baaef3509d0fcd1052e33a5fa9ade641788c1
ViktorRiegel <viktor.bekov26@gmail.com>
2024-01-04 15:45:04 +0200

    starting up

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+let app = express();
+let port = 3000;
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
