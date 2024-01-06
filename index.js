60aab3f0cc0ad2fc3debac48a94ddab14afa3361
ViktorRiegel <viktor.bekov26@gmail.com>
2024-01-06 14:48:14 +0200

    create view update

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
+});
+
+app.get("/update", (req, res) => {
+  res.render("update.ejs");
+});
+
+app.get("/view", (req, res) => {
+  res.render("view.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
