3aafc396216f337e8b2be2bf5e21ca6830e6a214
ViktorRiegel <viktor.bekov26@gmail.com>
2024-01-08 16:56:07 +0200

    view post

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 let app = express();
 let port = 3000;
 
+const postJSON =
+  '[{"id": "0001","name": "Chicken Taco","content": "content1" },  {"id": "0002","name": "Chicken 2","content": "content2" }]';
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let data;
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { post: data });
 });
 
 app.get("/create", (req, res) => {
@@ -17,6 +26,18 @@ app.get("/update", (req, res) => {
 
 app.get("/view", (req, res) => {
   res.render("view.ejs");
+});
+
+app.post("/post", (req, res) => {
+  switch (req.body.posts) {
+    case "post1":
+      data = JSON.parse(postJSON)[0];
+      break;
+    case "post2":
+      data = JSON.parse(postJSON)[1];
+      break;
+  }
+  res.redirect("/");
 });
 
 app.listen(port, () => {
